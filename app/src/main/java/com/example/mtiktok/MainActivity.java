f37155a917436687d6d73bd277762935ffdd25ac
Deep-Yellow <51702118+Deep-Yellow@users.noreply.github.com>
2020-07-19 17:17:14 +0800

    init

---
+++ app/src/main/java/com/example/mtiktok/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mtiktok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        open(R.id.button1, VideoDetail.class);
+
+    }
+
+    private void open(int buttonId, final Class<?> clz) {
+        findViewById(buttonId).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, clz));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mtiktok/VideoDetail.java
@@ -0,0 +1,43 @@
+package com.example.mtiktok;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class VideoDetail extends AppCompatActivity {
+    private Button buttonPlay;
+    private Button buttonPause;
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_detail);
+        setTitle("VideoView");
+
+        buttonPause = findViewById(R.id.buttonPause);
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoView.pause();
+            }
+        });
+
+        buttonPlay = findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoView.start();
+            }
+        });
+
+        videoView = findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse("https://jzvd.nathen.cn/video/2a101070-170bad88892-0007-1823-c86-de200.mp4"));
+    }
+
+}
